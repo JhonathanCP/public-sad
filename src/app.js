4a4a618eff54e2b8b883bfec9c7021e055d022d9
JhonathanCP <centtijhonathan@gmail.com>
2024-01-15 12:02:10 -0500

    first commit

--- src/app.js
+++ src/app.js
@@ -14,11 +14,18 @@ createRoles();
 createAdmin();
 
 //middlewares
-app.use(
-    cors({
-      // origin: "http://localhost:3000",
-    })
-);
+// app.use(
+//     cors({
+//       // origin: "http://localhost:3000",
+//     })
+// );
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 app.use(express.json());
 app.use(helmet());
 app.use(morgan('dev'));
