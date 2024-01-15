765a0111817f9bc516f36ad9d2020701454d5fef
JhonathanCP <centtijhonathan@gmail.com>
2024-01-15 12:11:30 -0500

    cambios0

--- src/database/database.js
+++ src/database/database.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-export const sequelize = new Sequelize('pbi_gctic', 'postgres', 'zxcvqwer159A-', {
-    host: 'localhost',
+export const sequelize = new Sequelize('sad_db', 'sad_db_user', 'PNSTusdJc7ZhjdKsENuCpA90xrApZc5j', {
+    host: 'postgres://sad_db_user:PNSTusdJc7ZhjdKsENuCpA90xrApZc5j@dpg-cmimd8ud3nmc73ckefig-a.oregon-postgres.render.com/sad_db',
     dialect: 'postgres'
 })
