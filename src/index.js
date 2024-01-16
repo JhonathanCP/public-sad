07fb0031b1e76b53951a9dc0e4be02d8b487eef6
JhonathanCP <centtijhonathan@gmail.com>
2024-01-16 08:20:39 -0500

    cambios

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import './models/Role.js'
 async function main() {
     try {
         await sequelize.authenticate();
-        await sequelize.sync({force: true})
+        await sequelize.sync()
         console.log('Database connected');
         app.listen(4000);
         console.log("Serven is listening on port", 4000);
