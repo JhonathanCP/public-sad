2fc5c0b6539575c2d84a1a36c3e7143509670d78
JhonathanCP <centtijhonathan@gmail.com>
2024-01-15 22:06:57 -0500

    cambios

--- src/database/database.js
+++ src/database/database.js
@@ -2,5 +2,8 @@ import { Sequelize } from "sequelize";
 
 export const sequelize = new Sequelize('sad_db', 'sad_db_user', 'PNSTusdJc7ZhjdKsENuCpA90xrApZc5j', {
     host: 'dpg-cmimd8ud3nmc73ckefig-a.oregon-postgres.render.com',
-    dialect: 'postgres'
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: true
+      }
 })
